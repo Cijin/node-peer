1db85f77c981d7e13d56f1075e303dce3724c189
cijin <cijininurhood@gmail.com>
2021-01-10 11:09:37 +0700

    updates run_tests.sh

--- Scripts/run_tests.sh
+++ Scripts/run_tests.sh
@@ -7,14 +7,14 @@ FORBIDDEN="console.log"
 
 ## Tests start here
 echo "${GREEN}Checking for Linting errors:${NC}"
-npm run lint
+npm run lint || exit 1
 
 echo "${GREEN}Checking for type errors:${NC}"
-npm run type-check
+npm run type-check || exit 1
 
 echo "${GREEN}Looking for forbidden references${NC}"
 git diff --cached --name-only | \
   grep -E $FILES_PATTERN | \
   xargs grep --color --with-filename -n $FORBIDDEN &&
-  echo "${RED}COMMIT REJECTED Found '$FORBIDDEN' references. Please remove them before commiting${NC}"
+  echo "${RED}COMMIT REJECTED Found '$FORBIDDEN' references. Please remove them before commiting${NC}" || exit 1
 
