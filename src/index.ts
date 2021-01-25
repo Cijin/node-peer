723422c8601aaa70614bd5e37545c8ca16db8db0
Cijin Cherian <cijininurhood@gmail.com>
2021-01-25 08:57:37 +0700

    Setting up a node http server (#6)
    * updates gitignore to ignore all files in dist
    
    * git ignore update
    
    * creates a conflict to resolve
    
    * update gitignore
    
    * update .gitignore
    
    * removing cached dist files
    
    * removing unnecessary gitignore lines
    
    * node server set up

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
-export function addNums(a: number, b: number): number {
-  // this is a comment
-  return a + b
-}
+import * as http from 'http'
+// npm packages
+import * as log4js from 'log4js'
+import * as yargs from 'yargs'
+
+// import pipingServer module here
+
+// parser options
+const parser = yargs.option('http-port', {
+  describe: 'Http Server Port',
+  default: 8080
+})
+
+// parse port arg
+const args = parser.parse(process.argv)
+const httpPort: number = args['http-port']
+
+// create logger
+const logger = log4js.getLogger()
+logger.level = 'info'
+
+// create a pipingServer and pass it to http server
+
+http.createServer().listen(httpPort, () => {
+  logger.info(`Http Server listening on Port: ${httpPort}`)
+})
+
+// catch uncaught exceptions and log them
+process.on('uncaughtException', (err) => {
+  logger.error('Uncaught exception: ', err)
+})
