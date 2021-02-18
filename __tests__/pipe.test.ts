20000dd5829efa014a79e6fe84163f4714e48bc0
cijin <cijininurhood@gmail.com>
2021-02-18 19:54:48 +0700

    updated tests, added script to run jest in watch mode

--- __tests__/pipe.test.ts
+++ __tests__/pipe.test.ts
@@ -94,4 +94,126 @@ describe('pipe.Server', () => {
     )
     expect(data.headers['content-type']).toStrictEqual(undefined)
   })
+
+  test('should pass senders Content-Type to reciever', async () => {
+    const reqPromise = thenRequest('GET', `${pipeURL}/mydataid`)
+
+    await thenRequest('POST', `${pipeURL}/mydataid`, {
+      headers: {
+        'content-type': 'text/plain'
+      },
+      body: 'this is content'
+    })
+
+    const data = await reqPromise
+
+    expect(data.headers['content-type']).toStrictEqual('text/plain')
+  })
+
+  test('should replace Content-Type from text/html to text/plain', async () => {
+    const reqPromise = thenRequest('GET', `${pipeURL}/mydataid`)
+
+    await thenRequest('POST', `${pipeURL}/mydataid`, {
+      headers: {
+        'content-type': 'text/html'
+      },
+      body: '<h1>this is content</h1>'
+    })
+
+    const data = await reqPromise
+
+    expect(data.headers['content-type']).toStrictEqual('text/plain')
+  })
+
+  test('should replace Content-Type from text/html; charset=utf8 to text/plain; charset=utf8', async () => {
+    const reqPromise = thenRequest('GET', `${pipeURL}/mydataid`)
+
+    await thenRequest('POST', `${pipeURL}/mydataid`, {
+      headers: {
+        'content-type': 'text/html; charset=utf8'
+      },
+      body: '<h1>this is content</h1>'
+    })
+
+    const data = await reqPromise
+
+    expect(data.headers['content-type']).toStrictEqual(
+      'text/plain; charset=utf8'
+    )
+  })
+
+  test('should send senders Content-Disposition to reciever', async () => {
+    const reqPromise = thenRequest('GET', `${pipeURL}/mydataid`)
+
+    await thenRequest('POST', `${pipeURL}/mydataid`, {
+      headers: {
+        'content-disposition': 'attachment; filename="myfile.txt"'
+      },
+      body: 'this is content'
+    })
+
+    const data = await reqPromise
+
+    expect(data.headers['content-disposition']).toStrictEqual(
+      'attachment; filename="myfile.txt"'
+    )
+  })
+
+  test('should be sent chunked data', async () => {
+    const sendReq = http.request({
+      host: 'localhost',
+      port,
+      method: 'POST',
+      path: '/mydataid'
+    })
+
+    sendReq.write('this is some')
+    sendReq.end(' content')
+
+    const data = await thenRequest('GET', `${pipeURL}/mydataid`)
+
+    expect(data.getBody('UTF-8')).toStrictEqual('this is some content')
+  })
+
+  test('sending data should work with PUT requests', async () => {
+    thenRequest('PUT', `${pipeURL}/mydataid`, {
+      body: 'some content here'
+    })
+
+    const data = await thenRequest('GET', `${pipeURL}/mydataid`)
+
+    expect(data.getBody('UTF-8')).toStrictEqual('some content here')
+    expect(data.headers['content-length']).toStrictEqual(
+      'some content here'.length.toString()
+    )
+  })
+
+  test('should handle multiple recievers', async () => {
+    const dataPromise1 = thenRequest('GET', `${pipeURL}/mydataid?n=3`)
+    const dataPromise2 = thenRequest('GET', `${pipeURL}/mydataid?n=3`)
+    const dataPromise3 = thenRequest('GET', `${pipeURL}/mydataid?n=3`)
+
+    thenRequest('POST', `${pipeURL}/mydataid?n=3`, {
+      body: 'this is content for all 3'
+    })
+
+    const [data1, data2, data3] = await Promise.all([
+      dataPromise1,
+      dataPromise2,
+      dataPromise3
+    ])
+
+    expect(data1.getBody('UTF-8')).toStrictEqual('this is content for all 3')
+    expect(data1.headers['content-length']).toStrictEqual(
+      'this is content for all 3'.length.toString()
+    )
+    expect(data2.getBody('UTF-8')).toStrictEqual('this is content for all 3')
+    expect(data2.headers['content-length']).toStrictEqual(
+      'this is content for all 3'.length.toString()
+    )
+    expect(data3.getBody('UTF-8')).toStrictEqual('this is content for all 3')
+    expect(data3.headers['content-length']).toStrictEqual(
+      'this is content for all 3'.length.toString()
+    )
+  })
 })
